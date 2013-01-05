1ec953ead02ab4cbf3fc30abefc49eede3146db0
unknown <vilango@VILANGO-MOBL1.gar.corp.intel.com>
2013-01-05 09:20:13 +0530

    Thumbnai URL near username

--- app/controllers/home_controller.js
+++ app/controllers/home_controller.js
@@ -5,7 +5,9 @@ var user = new User(req.user);
 
 user.getDisplayName();
 	
-	render('home.ejs', {user: req.user.displayName, title: 'Welcome '+req.user.displayName+ '!'}  );
+	render('home.ejs', {user: req.user.displayName, 
+		title: 'Welcome '+req.user.displayName+ '!',
+		thumbnail: req.user._json.thumbnail_url}  );
 
 
 });
